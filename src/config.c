083dd1c3be1b46a8f24834ce80b5100403032f46
Nicholas <nicholas.sidwell@gmail.com>
2026-01-02 21:05:55 -0800

    edit windows build for one-off

--- src/config.c
+++ src/config.c
@@ -122,7 +122,7 @@ void write_config(const config_params_s *conf) {
 
   SDL_Log("Writing config file to %s", config_path);
 
-#define INI_LINE_COUNT 50
+#define INI_LINE_COUNT 60
 #define INI_LINE_LENGTH 50
 
   // Entries for the config file
